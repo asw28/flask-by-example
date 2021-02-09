44c8a68be130c856387bff6cad37a00277e36454
aschw1 <as@biotx.ai>
2021-02-09 12:12:50 +0100

    added postgres dependencies to env

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,28 @@
 from app import app
 
-@app.route('/')
-def hello():
-    return "Root of flask-by-example"
-    
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Knobber'}
-    return render_template('index.html', title='Home', user=user)
+    reports = {'report1': 'slug1',
+               'report2': 'slug2'}
+    return render_template('index.html', title='index', user=user, reports=reports)
+
+@app.route('/slug1')
+def slug1():
+    user = {'username': 'Knobber'}
+    info = {'pagetitle': 'report1'}
+    return render_template('report.html', title=info['pagetitle'], info=info, user=user)
+
+@app.route('/slug2')
+def slug2():
+    user = {'username': 'Knobber'}
+    info = {'pagetitle': 'report2'}
+    return render_template('report.html', title=info['pagetitle'], info=info, user=user)    
+
 
 if __name__ == '__main__':
     app.run()
